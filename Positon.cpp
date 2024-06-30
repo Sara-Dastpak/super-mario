b1b703de0879bf797cb86a38d841f036304096cc
Sara Dastpak <96159156+Sara-Dastpak@users.noreply.github.com>
2024-06-30 08:47:28 +0330

    Create Positon.cpp

---
+++ Positon.cpp
@@ -0,0 +1,33 @@
+#include "Position.hpp"
+
+// Default constructor
+Position::Position() : x(0), y(0) {}
+
+// Parameterized constructor
+Position::Position(int x, int y) : x(x), y(y) {}
+
+// Accessor for x
+int Position::getX() const {
+    return x;
+}
+
+// Accessor for y
+int Position::getY() const {
+    return y;
+}
+
+// Mutator for x
+void Position::setX(int x) {
+    this->x = x;
+}
+
+// Mutator for y
+void Position::setY(int y) {
+    this->y = y;
+}
+
+// Move the position by deltaX and deltaY
+void Position::move(int deltaX, int deltaY) {
+    x += deltaX;
+    y += deltaY;
+}
